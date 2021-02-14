d7292cf83c5e3230e368c0ffea25be1f6a503852
Lauro Ribeiro <lauroribeirola@hotmail.com>
2021-02-14 20:38:48 -0300

    Make it so that independent of the size of squares, it will not change the grid size

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 const gridContainer = document.querySelector(".grid-container");
+const clearButton = document.querySelector("#clear-btn");
+const GRID_SIZE = 700;
+const GRID_BACKGROUND_COLOR = "rgb(230, 228, 228)";
 let gridItens = [];
 
+clearButton.addEventListener("click", clearGrid);
+
+function clearGrid(){
+    gridItens.forEach((gridItem) => {
+        gridItem.style.backgroundColor = GRID_BACKGROUND_COLOR;
+    });
+}
+
 function resizeGrid(size = 16){
-    gridContainer.style["grid-template-columns"]=`repeat(${size}, auto)`;
+    let gridItemSize = GRID_SIZE/size;
+    gridContainer.style["grid-template-columns"]=`repeat(${size}, ${gridItemSize}px)`;
+    gridContainer.style["grid-template-rows"]=`repeat(${size}, ${gridItemSize}px)`;
     for(let i = 0; i < size; i++){
         for(let j = 0; j < size; j++){
             const gridItem = document.createElement("div");
@@ -13,9 +26,9 @@ function resizeGrid(size = 16){
         gridItem.className = "grid-item";
         gridItem.addEventListener("mouseover", randomColor);
         gridContainer.appendChild(gridItem);
+        console.log(gridItem.className);
     });
 }
-resizeGrid();
 function randomColor(e){
     let a = Math.ceil(Math.random() * 255);
     let b = Math.ceil(Math.random() * 255);
@@ -23,3 +36,4 @@ function randomColor(e){
     e.target.style.backgroundColor = `rgb(${a},${b},${c})`;
     return;
 }
+resizeGrid();
